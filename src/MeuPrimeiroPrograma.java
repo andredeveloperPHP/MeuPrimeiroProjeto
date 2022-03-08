e2807a38ece3663957139fb6f6f8bc22e0f87881
Andre Panizza <97186379+andredeveloperPHP@users.noreply.github.com>
2022-03-08 16:31:44 -0300

    commit

--- src/MeuPrimeiroPrograma.java
+++ src/MeuPrimeiroPrograma.java
@@ -1,5 +1,17 @@
 public class MeuPrimeiroPrograma {
 
+    public static void main(String[] args) {
+        var number = 10;
+        var number1 = 10.3;
+        var login =  "ola";
+        login = "Mundo";
+        System.out.println(login);
+        System.out.println(number);
+        System.out.println(number1);
 
-    
+
+
+
+
+    }
 }
